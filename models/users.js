f86785216745ddc1dc11ed865322e6155a178001
abdizamedmo <abdizamedmo@gmail.com>
2023-09-27 21:28:05 +0300

    Users model created and also password in encryption and making beforeCreate action

--- models/users.js
+++ models/users.js
@@ -1,25 +1,33 @@
-const data = [
-    {
-        id: 1,
-        name: "SJNAJKNAD",
-        bio: "sjdnskjndkj"
-    },
-    {
-        id: 2,
-        name: "SJNAJKNAD",
-        bio: "sjdnskjndkj"
-    },
-    {
-        id: 3,
-        name: "SJNAJKNAD",
-        bio: "sjdnskjndkj"
-    },
-    {
-        id: 4,
-        name: "SJNAJKNAD",
-        bio: "sjdnskjndkj"
+const {Schema, model} = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+const schema = new Schema({
+
+    name: { type:String, required: true},
+    phone : {type:String, unique:true, required:true},
+    password: {type:String}
+})
+
+
+schema.pre("save", async function (next) {
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
     }
+    const salt = await bcrypt.genSalt(10);
+    this.password =  await bcrypt.hash(this.password, salt);
+    next();
+});
 
-]
+// hash, compare password
+schema.methods.hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt)
+}
 
-module.exports = data
+schema.methods.comparePassword = async (hashed, password) => {
+   return await bcrypt.compare(hashed, password)
+}
+
+module.exports = model("User", schema)
+
